54a7d02dd534b9922471562cd35ba3b8604979e7
John Geddes <jgeddes@thoughtworks.com>
2017-08-15 15:44:56 +1000

    [JG] Some test organisation

--- src/github/github.api.middleware.spec.js
+++ src/github/github.api.middleware.spec.js
@@ -13,25 +13,29 @@ import githubApiMiddleware from './github.api.middleware'
 Chai.use(ChaiThings)
 
 const middlewares = [githubApiMiddleware]
-var mockStore = ConfigureStore(middlewares)
-
-var mockAxios = new MockAdapter(Axios);
- 
-mockAxios.onGet('https://api.github.com/users/test/repos').reply(200, [
-    { id: 1, name: 'Test Repository' }
-  ]);
+var mockStoreFactory = ConfigureStore(middlewares)
 
 describe('Github.Api.Middleware', function () {
+  const middlewares = [githubApiMiddleware]
+  const mockStoreFactory = ConfigureStore(middlewares)
+  const testRepositories = [{ id: 1, name: 'Test Repository' }]
+  var store, mockAxios
+
+  before( () => {
+    store = mockStoreFactory({})
+    mockAxios = new MockAdapter(Axios)
+    mockAxios.onGet('https://api.github.com/users/test/repos').reply(200, testRepositories)
+  })
+
   it('Should dispatch FetchSuccess upon successful api call', async () => {
-    const initialState = {}
-    const store = mockStore(initialState)
+    
     const fetchRepositoriesAction = { type: types.FETCH_REPOSITORIES_START, username: 'test' }
 
     await store.dispatch(fetchRepositoriesAction)
 
     const actions = store.getActions()
 
-    const successPayload = setRepositories([{ id: 1, name: 'Test Repository' }])
+    const successPayload = setRepositories(testRepositories)
     expect(actions).to.include.something.that.deep.equals(fetchRepositoriesAction)
     expect(actions).to.include.something.that.deep.equals(successPayload)
   })
