336debfccab6a9777e7b64c51c6585a61fcc7578
John Geddes <jgeddes@thoughtworks.com>
2017-08-22 16:56:17 +1000

    [JG] WIP checkin

--- src/github/github.reducer.js
+++ src/github/github.reducer.js
@@ -3,16 +3,16 @@ import update from 'react-addons-update'
 
 const github = (state = [], action) => {
   switch (action.type) {
-    case types.FETCH_REPOSITORIES_SUCCESS:
-      return update(state, {
-        repositories: {$set: action.repositories},
-        loading: {$set: false}
-      })
     case types.FETCH_REPOSITORIES_START:
       return update(state, {
         repositories: {$set: []},
         loading: {$set: true},
         error: {$set: false}
+      })
+    case types.FETCH_REPOSITORIES_SUCCESS:
+      return update(state, {
+        repositories: {$set: action.repositories},
+        loading: {$set: false}
       })
     case types.FETCH_REPOSITORIES_FAILED:
       return update(state, {
